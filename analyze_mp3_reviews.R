1f354b410ea8247580a2cacc02c39f31fec8c531
evancchow <echow@princeton.edu>
2016-02-15 14:59:48 -0500

    preprocess text

--- analyze_mp3_reviews.R
+++ analyze_mp3_reviews.R
@@ -12,5 +12,24 @@ if (!exists("df.mp3")) {
   df.mp3[,c("rating", "helpfulVotesNum", "totalVotesNum")] <- apply(
     df.mp3[,c("rating", "helpfulVotesNum", "totalVotesNum")], 2, as.numeric)
   df.mp3$createDate <- as.Date(df.mp3$createDate)
+  
+  # convert classes
+  df.mp3[,c("fullText", "reviewTitle")] <- apply(df.mp3[,c("fullText", "reviewTitle")], 2, as.character)
+  df.mp3[,c("productName")] <- as.factor(df.mp3$productName)
+  
+  # preprocess text
+  require(qdap)
+  df.mp3[,c("fullText", "reviewTitle")] <- apply(df.mp3[,c("fullText", "reviewTitle")], 2, tolower)
+  df.mp3[,c("fullText", "reviewTitle")] <- data.frame(gsub("&quot;", "", as.matrix(df.mp3[,c("fullText", "reviewTitle")])),
+                                                      stringsAsFactors=F)
+  
+  # remove stopwords (leave out if you want the raw review text)
+  f_remove_stopwords <- function(x) {
+    return(paste(rm_stopwords(x, unlist=T, strip=T, apostrophe.remove=T), collapse=" "))
+  }
+  df.mp3$fullText <- as.character(sapply(df.mp3$fullText, f_remove_stopwords))
+  df.mp3$reviewTitle <- as.character(sapply(df.mp3$reviewTitle, f_remove_stopwords))
 }
+
+
 
